2b7f17b97c7ae2bd7198c59f7183bf40f7d6e445
Yusuke Hatanaka <arbalestimp@gmail.com>
2017-04-25 06:43:43 +0900

    Initialize redux-saga

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import createSagaMiddleware from 'redux-saga'
 import * as reducers from './reducers'
 import App from './containers/app'
 import { Home } from './containers/home'
 import { About } from './containers/about'
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
 
 // create store with reducers
 let store = createStore(
   combineReducers({
     ...reducers,
     routing: routerReducer,
-  })
+  }), 
+  applyMiddleware(sagaMiddleware)
 )
 
 // Create an enhanced history that syncs navigation events with the store
